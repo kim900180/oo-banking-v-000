c17b4e14e997989339619ddc98c0d5c6100cc635
Jae Yong Kim <kim900180@gmail.com>
2018-08-12 21:38:13 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -27,7 +27,7 @@ class Transfer
   def reverse_transfer
     if valid? && receiver.balance > amount && self.status == "complete"
       receiver.balance -= amount
-      sender.balance +=
+      sender.balance += amount
       self.status = "reversed"
     else
       reject_transfer
