a778da5a40a1ef6bbf357fc76dce7d0fa87b1402
Jae Yong Kim <kim900180@gmail.com>
2018-08-12 21:33:04 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,24 @@
 class Transfer
   # your code here
+  attr_reader :amount, :sender, :receiver
+  attr_accessor :status
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @status = "pending"
+    @amount = amount
+  end
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end
+
+  def execute_transaction
+  end
+
+  def reverse_transfer
+  end
+
+
 end
